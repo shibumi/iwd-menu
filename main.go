c2616155ab429c39b9aaf18ef8dae1e9fe92c31a
Christian Rebischke <chris@shibumi.dev>
2020-07-25 04:14:10 +0200

    add support for scanning

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -11,16 +13,35 @@ import (
 	"github.com/shibumi/iwd"
 )
 
+var errNoActiveStation error = errors.New("no active station for scanning available. Is your wifi adapter plugged in?")
+
+// getActiveStation returns the connected wifi station on success + err=nil.
+// On failure it will return an empty station object + an errNoActiveStation error.
+// A station is a connected wifi device like "wlan0".
+func getActiveStation(daemon iwd.Iwd) (activeStation iwd.Station, err error) {
+	for _, station := range daemon.Stations {
+		if station.State == "connected" {
+			return activeStation, nil
+		}
+	}
+	return iwd.Station{}, errNoActiveStation
+}
+
 func main() {
 	conn, err := dbus.SystemBus()
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	i := iwd.New(conn)
+	daemon := iwd.New(conn)
+	activeStation, err := getActiveStation(daemon)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	activeStation.Scan(conn)
 	openNetworks := map[int]iwd.Network{}
 	var index int = 0
-	for _, network := range i.Networks {
+	for _, network := range daemon.Networks {
 		if network.Type == "open" {
 			fmt.Println(index, "  ", network.Name)
 			openNetworks[index] = network
